08a184a038b9a057e9f717aa2882ce337061386c
Houston Braga <houstonliverboy123@gmail.com>
2023-11-22 11:45:40 -0300

    commit 34
    adicionamento do footer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 //botoes resumo
 
-let btnCursos = document.querySelectorAll('.btn-cursos');
+let btnCursos = document.getElementById('react');
 //aprender novamente o forEach
+
+btnCursos.addEventListener("click", () => {
+    alert('funcionou porra');
+});
+    
+
+
+const botoesCursos = [
+    'javascript',
+    'react',
+    'nodeJS'
+];
+
+botoesCursos.forEach((elemento, indice, completo) => console.log(elemento, indice, completo));
+
+
+
 
 
 
