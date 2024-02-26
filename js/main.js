b400973b415bda6fb034e9399a772a6bc45970fc
Houston Braga <houstonliverboy123@gmail.com>
2024-02-26 01:10:23 -0300

    commit 64
    1- troca de imagem em breve ao clicar no botao de trocar tema
    2- troca de imagens dos cursos ao clicar em determinado curso

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 //mudar de tema
-
+let emBreve = document.querySelectorAll('.photo-em-breve');
 const checkbox = document.getElementById("chk");
+
 checkbox.addEventListener("change", () => {
     document.body.classList.toggle('modo-light');
+    emBreve.forEach(index => {
+        if(document.body.classList.contains('modo-light')) {
+            index.src = 'img/em breve.png';
+        } else {
+            index.src = 'img/em breve dark.png'
+        }
+    });
 });
 
 //mudar a logo ao trocar o tema
@@ -22,22 +30,60 @@ function mudarLogo() {
 
 //botoes cursos
 
-let btnCursos = document.getElementById('react');
-//aprender novamente o forEach
+let btnCursos = {
+    react: document.getElementById('react'),
+    javascript: document.getElementById('javascript'),
+    nodejs: document.getElementById('nodejs'),
+    typescript: document.getElementById('typescript'),
+    uxui: document.getElementById('uxui'),
+    web: document.getElementById('web')
+};
 
-btnCursos.addEventListener("click", () => {
-    alert('funcionou porra');
+let conteudo = document.querySelectorAll('.img-curso');
+
+
+
+btnCursos.react.addEventListener("click", () => {
+    conteudo.forEach(element => {
+        element.src = 'img/reactcurso.png'
+    })
 });
+
+btnCursos.javascript.addEventListener("click", () => {
+    conteudo.forEach(element => {
+        element.src = 'img/javascript light.png'
+    })
+});
+
+btnCursos.nodejs.addEventListener("click", () => {
+    conteudo.forEach(element => {
+        element.src = 'img/nodecurso.png'
+    })
+});
+
+btnCursos.typescript.addEventListener("click", () => {
+    conteudo.forEach(element => {
+        element.src = 'img/typescript.png'
+    })
+});
+
+btnCursos.uxui.addEventListener("click", () => {
+    conteudo.forEach(element => {
+        element.src = 'img/uxui.png'
+    })
+});
+
+btnCursos.web.addEventListener("click", () => {
+    conteudo.forEach(element => {
+        element.src = 'img/webcurso.png'
+    })
+});
+
+
     
 
 
-const botoesCursos = [
-    'javascript',
-    'react',
-    'nodeJS'
-];
 
-botoesCursos.forEach((elemento, indice, completo) => console.log(elemento, indice, completo));
 
 //trocar imagem de EM BREVE
 
