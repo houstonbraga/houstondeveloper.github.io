4a99b8e5f07d0f90977d776abdba2b31dd5c19b5
Houston Braga <houstonliverboy123@gmail.com>
2024-08-09 16:51:21 -0300

    commit - inicio do gsap no portfolio

--- js/animationGSAP.js
+++ js/animationGSAP.js
@@ -1,9 +1,39 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+(function(){
+    const s=document.createElement("link").relList;
+    if(s&&s.supports&&s.supports("modulepreload"))return;
+    for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);
+    new MutationObserver(e=>{
+        for(const r of e)
+            if(r.type==="childList")
+                for(const a of r.addedNodes)
+                    a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)
+    }).observe(document,{childList:!0,subtree:!0});
+    function t(e){
+        const r={};
+        return e.integrity&&(r.integrity=e.integrity),
+        e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),
+        e.crossOrigin==="use-credentials"?r.credentials="include":
+        e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",
+        r
+    }
+    function i(e){
+        if(e.ep)return;
+        e.ep=!0;
+        const r=t(e);
+        fetch(e.href,r)
+    }
+})();
 
-ScrollTrigger.isTouch !== 1 && (
-    ScrollSmoother.create({
-        wrapper: ".bodie",
-        smooth: 1.5,
-        effects: true
-    })
-);
+ 
+ 
+     // Registra o plugin ScrollSmoother
+     gsap.registerPlugin(ScrollSmoother);
+
+     // Cria uma instância do ScrollSmoother
+     ScrollSmoother.create({
+        wrapper: ".wrapper",
+        content: ".content",
+        smooth: 1.8,  // Define a suavização da rolagem
+        effects: true // Habilita efeitos adicionais
+     });
+ 
